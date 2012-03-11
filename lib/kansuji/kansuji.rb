34b50c4414402cca259c551d38c3f4b9984af334
ymmtmsys <m2yamamoto@gmail.com>
2012-03-11 22:34:30 +0900

    compaction

--- lib/kansuji/kansuji.rb
+++ lib/kansuji/kansuji.rb
@@ -19,19 +19,12 @@ class Integer
   end
 
   def build_number_table(opts)
-    base_tbl = {
-      :trad => {
-          :new => %w(〇 壱 弐 参 四 伍 六 七 八 九),
-          :old => %w(零 壹 貳 参 肆 伍 陸 漆 捌 玖),
-      },
-      :basic => %w(〇 一 二 三 四 五 六 七 八 九),
-    }
-
-    tbl = base_tbl[:basic]
     if opts[:trad] == :new
-      tbl = base_tbl[:trad][:new]
+      tbl = %w(〇 壱 弐 参 四 伍 六 七 八 九)
     elsif opts[:trad] == :old
-      tbl = base_tbl[:trad][:old]
+      tbl = %w(零 壹 貳 参 肆 伍 陸 漆 捌 玖)
+    else
+      tbl = %w(〇 一 二 三 四 五 六 七 八 九)
     end
 
     tbl[0] = '〇' if opts[:zero] == :new
